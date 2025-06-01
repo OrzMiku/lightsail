fa1fa8d4239ae25d968c3845d05503497df730af
OrzMiku <orz@miku.show>
2025-06-01 17:59:44 +0800

    移除Proxy端

--- velocity/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -Xms1024M -Xmx1024M -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar velocity-3.4.0-SNAPSHOT-509.jar
